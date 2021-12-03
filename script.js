f90afc9a3cc76ecf887ebb021cce60f39b3ab461
vbhnem <90465522+vbhnem@users.noreply.github.com>
2021-12-03 15:36:42 -0800

    Attached event listeners, modified createSingleIndex and renderCreate functions,

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let contactList = [
+const mainDiv = document.querySelector(".main")
+
+const contactList = [
     {
       name: "Roberta Dobbs",
       phone: "778-555-1234",
@@ -13,167 +15,259 @@ let contactList = [
     }
   ]
 
+const Contacts = document.querySelector("#contactshome")
+
+Contacts.addEventListener("click", (e) => {
+    cleanUpIndex()
+    renderIndex(contactList)
+    e.preventDefault()
+})
+
+const Create_New_Contact = document.querySelector("#newcontact")
+
+Create_New_Contact.addEventListener("click", (e) => {
+    cleanUpIndex()
+    renderCreate()
+    e.preventDefault()
+})
+
+
+
 function cleanUpIndex() {
-    const parent = document.querySelector(".main")
-    const child = document.querySelectorAll(".contact")
-
-    while (child) {
-       parent.removeChild(child)
-   }
-
+    while (mainDiv.lastChild){
+        mainDiv.lastChild.remove()
+    }
 }
 
-function createSingleIndex(contactList) {
-    const a = document.createElement("a")
-    a.herf = "page3.html"
+function createSingleIndex(contact) {
+    const aa = document.createElement("a")
+    const Div = document.createElement("div") 
+    Div.classList.add("contact")
 
-    const div = document.createElement("div")
-    div.setAttribute("class", "contact")
-    a.appendChild(div)
+    const para = document.createElement("p")
+    const text_name = document.createTextNode(contact.name)
+    para.append(text_name)
 
-    const p = document.createElement("p")
-    p.innerHTML = contactList.name
-    div.appendChild(p)
+    Div.append(para)
+    aa.append(Div)
+    //aa.href = "page3.html"
+    
+    aa.addEventListener("click", (e) => {
+        cleanUpView()
+        const name = contact.name
+        var storage = -1
+        for (var i=0; i<contactList.length; i++ ){
+            if (name == contactList[i].name) {
+                
+                storage = i
+                break
+            }
+        }
+        if (storage != -1){
+            cleanUpIndex()
+            renderView(contactList[storage])
+        }
+        
+        e.preventDefault()
+    })
+
+    return aa;
+
 };
 
 
-function renderIndex(contactList) {
-    const main = document.querySelector(".main")
-
-    for (var i = 0; i < contactList.length; i += 1) {
-        var singlecontact = contactList[i];
-        main.append(createSingleIndex(singlecontact))
-    }
-
+function renderIndex(contactArray) {
+    contactArray.forEach ((ele) => {
+        mainDiv.append(createSingleIndex(ele))
+    })
+    
 }
 
 function cleanUpView() {
-    const parent = document.querySelector(".contactinfo")
-    const name = document.querySelector(".contactname")
-    const email = document.querySelector(".contactemail")
-    const phone = document.querySelector(".contactphone")
-    const address = document.querySelector(".contactaddress")
-
-    if (name) {
-        parent.remove(name)
-    }
-    if (email) {
-        parent.remove(email)
-    }
-    if (phone) {
-        parent.remove(phone)
-    }
-    if (address) {
-        parent.remove(address)
+    while (mainDiv.lastChild) {
+        mainDiv.lastChild.remove()
     }
 
-};
+}
 
-function renderView(contactList) {
-    const div =  document.createElement("div")
-    div.classList.add("contactinfo")
+function renderView(contact) {
+    cleanUpView()
 
-    const div1 = document.createElement("div")
-    div1.classList.add("contactname")
-    div1.innerHTML = contactList.name
-    div.appendChild(div1)
+    const contactInfo = document.createElement("div")
+    contactInfo.classList.add("contactinfo")
 
-    const div2 = document.createElement("div")
-    div2.classList.add("contactemail")
-    div2.innerHTML = contactList.email
-    div.appendChild(div2)
+    const contactName = document.createElement("div")
+    contactName.classList.add("contactname")
+    const text_name = document.createTextNode(contact.name)
+    contactName.append(text_name)
 
-    const div3 = document.createElement("div")
-    div3.classList.add("contactphone")
-    div3.innerHTML = contactList.phone
-    div.appendChild(div3)
+    const Img = document.createElement("img")
+    Img.src = "./img/profile.jpg"
+    Img.classList.add("profilepic")
+    contactName.append(Img)
+        
+    const contactEmail = document.createElement("div")
+    contactEmail.classList.add("contactemail")
+    const text_email = document.createTextNode("email: " + contact.email)
+    contactEmail.append(text_email)
 
-    const div4 = document.createElement("div")
-    div4.classList.add("contactaddress")
-    div4.innerHTML = contactList.address
-    div.appendChild(div4)
+    const contactPhone = document.createElement("div")
+    contactPhone.classList.add("contactphone")
+    const text_phone = document.createTextNode("cell: " + contact.phone)
+    contactPhone.append(text_phone)
 
-};
+    const contactAddress = document.createElement("div")
+    contactAddress.classList.add("contactaddress")
+    const text_address = document.createTextNode("address: " + contact.address)
+    contactAddress.append(text_address)
 
-function cleanUpCreate() {
-    const parent = document.querySelector(".form")
-    const child = document.querySelectorAll(".inputcontainer")
-
-    while (child) {
-        parent.remove(child)
-    }
-};
-
-function renderCreate(contactList) {
-    const div = document.createElement("form")
-
-    const div1 = document.createElement("div")
-    div1.setAttribute("class", "inputcontainer")
-    div.appendChild(div1)
-
-    const input1 = document.createElement("input")
-    input1.setAttribute("type", "text")
-    input1.setAttribute("id", "contactname")
-    input1.setAttribute("name", "contactname")
-    input1.setAttribute("placeholder", contactList.name)
-    div1.appendChild(input1)
+    const buttons = document.createElement("div")
+    buttons.classList.add("buttons")
 
     const button1 = document.createElement("button")
-    button1.setAttribute("class", "extrafield")
-    button1.setAttribute("id", "extranamefield")
-    button1.setAttribute("name", "extranamefield")
-    input1.appendChild(button1)
-//--------------------
-    const div2 = document.createElement("div")
-    div2.setAttribute("class", "inputcontainer")
-    div.appendChild(div2)
-
-    const input2 = document.createElement("input")
-    input2.setAttribute("type", "tel")
-    input2.setAttribute("id", "contactphone")
-    input2.setAttribute("name", "contactphone")
-    input2.setAttribute("placeholder", contactList.phone)
-    div2.appendChild(input2)
+    button1.classList.add("button")
+    button1.classList.add("edit")
+    button1.value = "Edit"
+    const text1 = document.createTextNode("Edit")
+    button1.append(text1)
+    buttons.append(button1)
 
     const button2 = document.createElement("button")
-    button2.setAttribute("class", "extrafield")
-    button2.setAttribute("id", "extraphonefield")
-    button2.setAttribute("name", "extraphonefield")
-    input2.appendChild(button2)
-//--------------
-    const div3 = document.createElement("div")
-    div3.setAttribute("class", "inputcontainer")
-    div.appendChild(div3)
+    button2.classList.add("button")
+    button2.classList.add("close")
+    button2.value = "Close"
+    const text2 = document.createTextNode("Close")
+    button2.append(text2)
+    buttons.append(button2)
 
-    const input3 = document.createElement("input")
-    input3.setAttribute("type", "text")
-    input3.setAttribute("id", "contactaddress")
-    input3.setAttribute("name", "contactaddress")
-    input3.setAttribute("placeholder", contactList.address)
-    div3.appendChild(input3)
 
-    const button3 = document.createElement("button")
-    button3.setAttribute("class", "extrafield")
-    button3.setAttribute("id", "extraaddressfield")
-    button3.setAttribute("name", "extraaddressfield")
-    input3.appendChild(button3)
-//-----------------
-    const div4 = document.createElement("div")
-    div4.setAttribute("class", "inputcontainer")
-    div.appendChild(div4)
+    contactInfo.append(contactName)
+    contactInfo.append(contactEmail)
+    contactInfo.append(contactPhone)
+    contactInfo.append(contactAddress)
+    contactInfo.append(buttons)
 
-    const input4 = document.createElement("input")
-    input4.setAttribute("type", "email")
-    input4.setAttribute("id", "contactemail")
-    input4.setAttribute("name", "contactemail")
-    input4.setAttribute("placeholder", contactList.email)
-    div4.appendChild(input4)
+    mainDiv.append(contactInfo)
 
-    const button4 = document.createElement("button")
-    button4.setAttribute("class", "extrafield")
-    button4.setAttribute("id", "extraemailfield")
-    button4.setAttribute("name", "extraemailfield")
-    input4.appendChild(button4)
+    button2.addEventListener("click", (e) => {
+        cleanUpIndex()
+        renderIndex(contactList)
+        e.preventDefault()
+    })
+}
 
-};
+function cleanUpCreate() {
+    while (mainDiv.lastChild) {
+        mainDiv.lastChild.remove()
+    }
+}
+
+const parser = new DOMParser()
+
+const inputId = [
+    "contactname", "contactphone", "contactaddress", "contactemail"
+]
+const placeHolder = [
+    "Contact Name", " Contact Phone", " Contact Address", " Contact Email"
+]
+
+const type = [
+    "text", "tel", "text", "email"
+]
+const extraFieldTxt = "<button class='extrafield' id='extraaddressfield' name='extraaddressfield'></button>"
+
+function renderCreate() {
+    cleanUpCreate()
+
+    let contactEditDiv = document.createElement("div")
+    contactEditDiv.classList.add("contactedit")
+
+    let contactImgDiv = document.createElement("div");
+    contactImgDiv.classList.add("contactimg")
+    const imgTag = document.createElement("img")
+    imgTag.classList.add("profilepic")
+    imgTag.src = "./img/profile.jpg"
+    imgTag.alt = "Profile picture"
+
+    contactImgDiv.append(imgTag)
+    contactEditDiv.append(contactImgDiv)
+
+    let formDiv = document.createElement("div")
+    formDiv.classList.add("form")
+
+    let myForm = document.createElement("form")
+
+    for (let i = 0; i <4; i++) {
+        const inputContainerDiv = document.createElement("div")
+        
+        inputContainerDiv.classList.add("inputcontainer")
+        
+        const inputTxt = `<input type="${type[i]}" id="${inputId[i]}" name="${inputId[i]}" placeholder="${placeHolder[i]}"></input>`
+        
+        inputContainerDiv.innerHTML = inputTxt
+
+        let btn = parser.parseFromString(extraFieldTxt, "text/html").body.firstChild
+        btn.textContent = "+"
+        inputContainerDiv.append(btn)
+
+        myForm.append(inputContainerDiv)
+    }
+
+    let btnDiv =document.createElement('div')
+    btnDiv.classList.add("buttons")
+   
+    const button1 = document.createElement("button")
+    button1.classList.add("button")
+    button1.classList.add("save")
+    const text_save = document.createTextNode("Save")
+    button1.append(text_save)
+    btnDiv.append(button1)
+
+    const button2 = document.createElement("button")
+    button2.classList.add("button")
+    button2.classList.add("cancel")
+    const text_cancel = document.createTextNode("Cancel")
+    button2.append(text_cancel)
+    btnDiv.append(button2)
+    
+    myForm.append(btnDiv)
+
+    formDiv.append(myForm)
+
+    contactEditDiv.append(formDiv)
+
+    mainDiv.append(contactEditDiv)
+
+    button2.addEventListener("click", (e) => {
+        cleanUpIndex()
+        renderIndex(contactList)
+        e.preventDefault()
+    })
+
+    button1.addEventListener("click", (e) => {
+        const contactName = document.querySelector("#contactname")
+        const contactPhone = document.querySelector("#contactphone")
+        const contactAddtess = document.querySelector("#contactaddress")
+        const contactEmail = document.querySelector("#contactemail")
+
+        let contact = {
+            name: contactName.value,
+            phone: contactPhone.value,
+            address: contactAddtess.value,
+            email: contactEmail.value
+        }
+        
+        contactList.push(contact)
+        cleanUpView()
+        renderIndex(contactList)
+        e.preventDefault()
+    })
+
+    window.addEventListener("load", (e) => {
+        cleanUpIndex()
+        renderIndex(contactList)
+        e.preventDefault()
+    })
+
+}
 
